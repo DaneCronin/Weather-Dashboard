1a15eb72a3686f66c16e28ca6d10c0605fb60a15
Dane Cronin <danecronin@gmail.com>
2022-08-20 14:35:12 -0600

    added psuedo code to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Declare variables for city
+
+
+// Create function to fetch data from OpenWeather API
+
+
+
+// Display current city weather to site
+
+// Display 5-day Forecast of current city
+
+// Store cities searched to localStorage and show as buttons beneath search
+
