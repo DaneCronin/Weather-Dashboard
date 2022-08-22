7439a92e2cbf1e38f923b302a66f1ec8973b8c78
Dane Cronin <danecronin@gmail.com>
2022-08-21 19:20:34 -0600

    changed API Key to its own variable, addedd event listener for button click on search. added imperial unit measuerment to query parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,37 @@
+
+//API Key for OpenWeather
+const APIKey = "202c298807f99a13122415d39ef0b143";
+
 // Declare variables for city
 var cityFormEl = document.querySelector("#search-form"); // Variable for the search city form element
-var cityNameInput = document.querySelector("#city-search"); //Variable to hold the searched city name
+var cityNameSearch = document.querySelector("#city-search"); //Variable for the searched city name
+var cityName = document.querySelector("#city-name"); //Variable to display searched City's name in current weather element 
 var currentWeatherEl = document.querySelector("#current-weather"); //Variable to hold current weather
 var fiveDayEl = document.querySelector("#extended-forecast"); //Variable to hold the 5-day extended forecast
-var cities = [];  //Store cities in an array
+var cityHistory = [];  //Store cities in an array
+
+
+
+// Create Click Event handler for search form
+var formSubmitHandler= function (event) {
+    event.preventDefault();
+
+
+    var city = cityNameSearch.value.trim();
+    
+    if (city) {
+        getCity(city);
+        cityNameSearch.value = "";
+    } else {
+        alert("Please enter city name!");
+    }
+    console.log("city searched");
+};
+
 
 //Create function to get city name, date and time
 var getCity = function (city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city.name + "&appid=202c298807f99a13122415d39ef0b143";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
         fetch(apiUrl)
         .then(function (response) {
             return response.json();
@@ -22,7 +46,7 @@ var getCity = function (city) {
 
             cityName.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ")";
 
-            weather(data);
+           // weather(data);
         })
 
 }
@@ -30,7 +54,7 @@ var getCity = function (city) {
 
 // Create function to fetch data from OpenWeather API
 var getWeatherInfo = function() {
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + data.coord.lat + "&units=imperial&cnt=5&appid=202c298807f99a13122415d39ef0b143";
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?lat=" + data.coord.lat + "&units=imperial&cnt=5&appid=" + APIKey;
     //make fetch request to get weather info 
     fetch(weatherUrl)
         .then(function(response) {
@@ -42,27 +66,35 @@ var getWeatherInfo = function() {
    
 };
 
-// Create Click Event handler for search form
-var cityNameInput = function (event) {
-    event.preventDefault();
-
-    console.log("city searched");
-
-    var city = search.value.trim().touppercase;
-    
-    if (city) {
-        getCity(city);
-        search.value = "";
-    } else {
-        alert("Please enter city name!");
-    }
-};
 
 
 
 // Display current city weather to site
+var displayWeather = function(weather) {
+    console.log("weather data");
+
+
+    //check for returned weather data from api
+    if (weather.length === 0) {
+        currentWeatherEl.textContent = "No weather data found.";
+        return;
+    }
+
+    //display temp
+
+    //display wind speed
+
+    //display humidity
+
+    //display UV index
+
+
+};
+
 
 // Display 5-day Forecast of current city
 
 // Store cities searched to localStorage and show as buttons beneath search
 
+//Add event listener for button click on search
+cityFormEl.addEventListener("submit", formSubmitHandler);
