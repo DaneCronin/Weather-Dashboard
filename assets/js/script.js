b99c39d30249b78d7efdb0e8caa7a7ae4405fd20
Dane Cronin <danecronin@gmail.com>
2022-08-23 16:26:56 -0600

    trying to change styling and get five-day forecast to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var cityNameSearch = document.querySelector("#city-search"); //Variable for the 
 var weatherEl = document.querySelector(".weather")// Weather div for current weather
 var cityName = document.querySelector("#city-name"); //Variable to display searched City's name in current weather element 
 var currentWeatherEl = document.querySelector("#current-weather"); //Variable to hold current weather
-var fiveDayEl = document.querySelector("#extended-forecast"); //Variable to hold the 5-day extended forecast
+var fiveDayEl = document.querySelector(".extended-forecast"); //Variable to hold the 5-day extended forecast
 var searchHistoryEl = document.querySelector(".btn"); //Variables for button to hold searched-for cities
 var historyCardEl = document.querySelector(".history"); //Variable for container for searched city buttons
 var cityHistory = [];  //Store cities in an array
@@ -29,11 +29,7 @@ var formSubmitHandler= function (event) {
         //Save searcehd city names into local storage and display in savedCityButtons
         cityHistory.push(city);
         localStorage.setItem("weatherSearch", JSON.stringify(cityHistory));
-        // var searchHistoryEl = document.createElement('button');
-        // searchHistoryEl.className = "btn";
-        // searchHistoryEl.setAttribute("data-city", city)
-        // searchHistoryEl.innerHTML = city;
-        // savedCityButtonsEl.appendChild(searchHistoryEl);
+    
         
 
         loadHistory(city);
@@ -146,14 +142,15 @@ var displayCurrentWeather = function(data) {
 
 
 //Display 5-day Forecast of current city
-var forecast = function (weather) {
-    var extendedForecastArray = weather.daily;
+var forecast = function (data) {
+    var extendedForecastArray = data.list[i];
+    console.log(data);
     // loop over weather data 
     for (var i=0; i < extendedForecastArray; i++) {
 
         var date = (today.getMonth() + 1) + '/' + (today.getDate()) + '/' + today.getFullYear();
-        var weatherIcon = extendedForecastArray[i].weather[0].icon;
-        var weatherDescription = extendedForecastArray[i].weather[0].description;
+        var weatherIcon = extendedForecastArray[i].weather[i].icon;
+        var weatherDescription = extendedForecastArray[i].weather[i].description;
         weatherIconLink = "<img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='" + weatherDescription + "' title='" + weatherDescription + "'  />";
         var dayEl = document.createElement("div");
         dayEl.className = "day";
@@ -162,7 +159,9 @@ var forecast = function (weather) {
             "<p>Temp:" + extendedForecastArray[i].temp.day.toFixed(1) + "°F</p>" +
             "<p>Humidity: " + extendedForecastArray[i].humidity + "%</p>"
 
-        fiveDayEl.appendChild(dayEl);
+        
+        fiveDayEl.append(dayEl);
+
     }
 }
 //load cities searched to localStorage and show as buttons beneath search
